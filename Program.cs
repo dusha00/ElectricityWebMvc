0831f9bb98e6928fd7aac01d638c71d2e543d345
Bob Du <DUXBX@legal.regn.net>
2019-09-19 14:32:39 +0800

    add git ignore

--- Program.cs
+++ Program.cs
@@ -19,6 +19,6 @@ namespace ElectricityWebMvc
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseUrls("http://*:7000");
     }
 }
